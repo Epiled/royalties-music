a8e4e762ce08e1d83fbb292f653d546c58ec85c5
Felipe De Andrade <felipe.deam98@gmail.com>
2024-02-08 15:12:38 -0300

    Início Menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+
+const Menu = () => {
+  return (
+    <header>
+      
+      <nav>
+        <Link to={'/'}></Link>
+        <Link to={'/'}></Link>
+        <Link to={'/'}></Link>
+        <Link to={'/'}></Link>
+      </nav>
+      
+    </header>
+  )
+}
+
+export default Menu
