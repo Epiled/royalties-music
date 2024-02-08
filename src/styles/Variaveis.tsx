267494f6fe6c4af74c977551ef08c176d1cc3040
Felipe De Andrade <felipe.deam98@gmail.com>
2024-02-08 15:02:31 -0300

    PreCode Concluído

--- src/styles/Variaveis.tsx
+++ src/styles/Variaveis.tsx
@@ -2,7 +2,13 @@ import { createGlobalStyle } from "styled-components";
 
 const Variaveis: React.FC = createGlobalStyle`
   :root {
+    --lighter: #FFFFFF;
+    --darker: #000000;
 
+    --color-main: #6A00FF;
+    --color-secondary: #FFC900;
+
+    --color-txt: #1A1A1A;
   }
 `
 
